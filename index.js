c6b5b51dfe48a81d0715160448e934d7464e6bf9
Suraj Singh <suraj.singh@minoanexperience.com>
2024-01-16 18:14:26 +0530

    store refresh token

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 const routes = require("./routes")
 // Create an instance of the express application
 const app = express();
+const mongoose = require('mongoose');
 
 // Use body-parser middleware to parse JSON and URL-encoded bodies
 app.use(bodyParser.json());
@@ -14,6 +15,18 @@ app.use(morgan());
 app.use(cors());
 
 app.use('/', routes);
+
+mongoose.connect('mongodb+srv://tqBFad4UAAkYxhKK:tqBFad4UAAkYxhKK@cluster0.fzwy1.mongodb.net/AllCRMEvents?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+})    .then(() => {
+    console.log('Connected to MongoDB');
+
+    // Additional setup or actions after successful connection
+})
+    .catch(error => {
+        console.error('Error connecting to Mo' +
+            'goDB:', error.message);
+    });
 
 
 // Specify the port for the server to listen on
